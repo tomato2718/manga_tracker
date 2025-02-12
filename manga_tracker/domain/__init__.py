3ec8a500209c508a8ae8dcb80e0123cafb457fd9
tomato2718 <qweasdzxc030@gmail.com>
2025-02-12 17:34:36 +0800

    Modify: Expose common Result class

--- manga_tracker/domain/__init__.py
+++ manga_tracker/domain/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Manga", "MangaRepository", "Crawler", "CrawlerFail"]
+__all__ = ["Manga", "MangaRepository", "Crawler", "CrawlerFail", "Result"]
 
+from ._common import Result
 from ._entity import Manga
 from ._repository import MangaRepository
 from ._service import Crawler, CrawlerFail
