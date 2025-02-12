add367928937ddbc51a52efe7e9bade9967dc284
tomato2718 <qweasdzxc030@gmail.com>
2025-02-12 17:24:02 +0800

    Modify: Use Result as return for Crawler Protocol

--- manga_tracker/domain/__init__.py
+++ manga_tracker/domain/__init__.py
@@ -1,5 +1,5 @@
-__all__ = ["Manga", "MangaRepository", "Crawler"]
+__all__ = ["Manga", "MangaRepository", "Crawler", "CrawlerFail"]
 
 from ._entity import Manga
 from ._repository import MangaRepository
-from ._service import Crawler
+from ._service import Crawler, CrawlerFail
--- manga_tracker/domain/_service.py
+++ manga_tracker/domain/_service.py
@@ -1,10 +1,17 @@
-__all__ = ["Crawler"]
+__all__ = ["Crawler", "CrawlerFail"]
 
 from collections.abc import Iterable
+from enum import Enum
 from typing import Protocol
 
+from ._common import Result
 from ._entity import Manga
 
 
 class Crawler(Protocol):
-    def crawl(self) -> Iterable[Manga]: ...
+    def crawl(self) -> Result[Iterable[Manga], "CrawlerFail"]: ...
+
+
+class CrawlerFail(Enum):
+    UNACCESSABLE = 0
+    FAIL_TO_PARSE_RESOURCE = 1
